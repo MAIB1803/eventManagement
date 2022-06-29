f8b45d57c8365abdd09e69006d2dd1e28d73b8bc
MAIB1803 <47940465+MAIB1803@users.noreply.github.com>
2022-06-29 17:11:47 +0530

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'bd941faf59cd90ae47f8846ec7d2e8d0b73a07be',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bd941faf59cd90ae47f8846ec7d2e8d0b73a07be',
+    ),
+  ),
+);
